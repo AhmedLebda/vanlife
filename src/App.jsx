61a2b01b9b4d102ed7058487f186c8db62936b8d
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2024-04-18 14:06:45 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="text-slate-800 text-5xl font-extrabold">
+                Testing...
+            </div>
+        </>
+    );
+}
+
+export default App;
